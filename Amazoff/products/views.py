6a4fa206a53c60763a4ef4efb1a79d07ecdae53f
Pranav132 <mais.06pranav@gmail.com>
2021-10-19 11:30:20 +0530

    added product and tested

--- Amazoff/products/views.py
+++ Amazoff/products/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.models import Product
 
 # Create your views here.
 
 
 def product(request):
+    products = Product.objects.all().filter(inventory = 1)
+    for product in products:
+        print(product.tags)
     return HttpResponse("<h1>Hello, these are our products</h1>")
