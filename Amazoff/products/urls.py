acb53429209d0cdcd7909851b32aba18a1821d1c
Pranav132 <mais.06pranav@gmail.com>
2021-10-24 13:51:35 +0530

    Created page for all products and for each individual product

--- Amazoff/products/urls.py
+++ Amazoff/products/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product, name='product_view'),
+    path('products/', views.products, name='products_view'),
+    path('products/<int:product_id>/', views.product, name='product_page'),
     path("", views.index, name="index"),
     path("home/", views.index, name="index"),
     path("search/", views.search, name="product_search"),
--- Amazoff/products/views.py
+++ Amazoff/products/views.py
@@ -4,19 +4,29 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "index.html")
 
 
-def product(request):
-    products = Product.objects.all().filter(inventory=1)
+def products(request):
+    products = Product.objects.all()
+    prods = []
     for product in products:
-        print(product.tags)
-    return HttpResponse("<h1>Hello, these are our products</h1>")
+        prods.append([product.id, product.name, product.site_title, product.price,
+                      product.description, product.tags, product.popularity])
+
+    return render(request, "products.html", {"products": products})
+
+
+def product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, "product_page.html", {"product": product})
+
 
 def search(request):
     if request.method == 'POST':
-        search= request.POST['searched']
+        search = request.POST['searched']
         product = Product.objects.filter(name__icontains=search)
-        return render (request, 'product_search.html', {"product":product, "search":search})
+        return render(request, 'product_search.html', {"product": product, "search": search})
         # return HttpResponse("<h1>Search works only.</h1>")
