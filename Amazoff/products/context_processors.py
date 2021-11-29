f5e4d913a6be7cad31bd627774504c888341b89c
angadtrehan <ast.angad23@gmail.com>
2021-11-29 12:55:09 +0530

    test

--- Amazoff/products/context_processors.py
+++ Amazoff/products/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Product, Product_Categories, Tags, subcategories
+from .models import Product, Product_Categories, Tags, subcategories, Wishlist, WishlistItem
 
 
 def autocomplete_processor(request):
@@ -12,3 +12,14 @@ def autocomplete_processor(request):
         "template_subcategories": sub_categories,
         "template_tags": product_tags
     }
+
+
+def wishlist_checker(request):
+    wishlistItems = {}
+    if request.user.is_authenticated:
+        wishlist = Wishlist.objects.filter(user=request.user).first()
+        wishlist_Items = WishlistItem.objects.filter(wishlist=wishlist)
+        for item in wishlist_Items:
+            wishlistItems[item.product.name] = True
+
+    return {"template_wishlistItems": wishlistItems}
